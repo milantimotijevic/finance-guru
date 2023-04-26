485923ec4c095057117842b04ec722e1423e45c5
Milan Timotijevic <mtimo101289@gmail.com>
2023-04-26 15:30:35 +0200

    fix rounding

--- src/service/Transaction.js
+++ src/service/Transaction.js
@@ -23,14 +23,19 @@ const getCostStatistics = async (userId) => {
 
             debits.count += 1;
             debits.categories[debitCategory].count += 1;
-            debits.total = Math.round((debits.total + Math.abs(transaction.amount)) * 100) / 100;
-            debits.categories[debitCategory].total = Math.round((debits.categories[debitCategory].total + Math.abs(transaction.amount)) * 100) / 100;
+            debits.total += Math.abs(transaction.amount);
+            debits.categories[debitCategory].total += Math.abs(transaction.amount);
         }
     });
 
-
-
     debits.average = Math.round(debits.total / debits.count * 100) / 100;
+    debits.total = Math.round(debits.total * 100) / 100;
+
+    Object.keys(debits.categories).forEach(categoryKey => {
+        const debitCategory = debits.categories[categoryKey];
+        debitCategory.average = Math.round(debitCategory.total / debitCategory.count * 100) / 100;
+        debitCategory.total = Math.round(debitCategory.total * 100) / 100;
+    });
 
     return debits;
 };
