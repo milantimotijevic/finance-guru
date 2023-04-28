dda4178b76054513fa4d778146c2d2ac6cfe024a
Milan Timotijevic <mtimo101289@gmail.com>
2023-04-28 18:37:55 +0200

    update unit test

--- test/gateway/BasiqApi.test.js
+++ test/gateway/BasiqApi.test.js
@@ -8,7 +8,7 @@ jest.mock('axios');
 
 test('getTransactions', async () => {
     // mock login
-    axios.mockResolvedValue({
+    axios.mockResolvedValueOnce({
         data: {
             access_token: 'testtoken',
             token_type: 'Bearer',
@@ -17,7 +17,7 @@ test('getTransactions', async () => {
     });
 
     // mock fetching transaction list
-    axios.mockResolvedValue({
+    axios.mockResolvedValueOnce({
         data: {
             type: 'list',
             count: 1,
@@ -83,9 +83,9 @@ test('getTransactions', async () => {
     }]);
 });
 
-test('getTransactions', async () => {
+test('getTransactions passes with 3 failed fetches', async () => {
     // mock login
-    axios.mockResolvedValue({
+    axios.mockResolvedValueOnce({
         data: {
             access_token: 'testtoken',
             token_type: 'Bearer',
@@ -93,50 +93,11 @@ test('getTransactions', async () => {
         }
     });
 
-    // mock fetching transaction list
-    axios.mockResolvedValue({
-        data: {
-            type: 'list',
-            count: 1,
-            size: 1,
-            data: [
-                {
-                    type: 'transaction',
-                    id: '67985501-f0b1-489b-a460-7662e85be689',
-                    status: 'posted',
-                    description: 'Contribution - Super Guarantee',
-                    amount: '609.69',
-                    account: '44f7e034-a209-4dd0-ab6c-ef7f6f42440a',
-                    balance: '0.00',
-                    direction: 'credit',
-                    class: 'transfer',
-                    institution: 'AU00000',
-                    connection: 'f8c4644e-a93d-4ea7-b1ff-59f595cda027',
-                    enrich: null,
-                    transactionDate: '',
-                    postDate: '2023-04-13T00:00:00Z',
-                    subClass: {
-                        title: 'Superannuation Funds',
-                        code: '633'
-                    },
-                    links: {
-                        self: 'https://au-api.basiq.io/users/c9f76ad8-491d-4a81-b68b-653672dfa6e7/transactions/67985501-f0b1-489b-a460-7662e85be689',
-                        account: 'https://au-api.basiq.io/users/c9f76ad8-491d-4a81-b68b-653672dfa6e7/accounts/44f7e034-a209-4dd0-ab6c-ef7f6f42440a',
-                        institution: 'https://au-api.basiq.io/institutions/AU00000',
-                        connection: null
-                    }
-                }
-            ]
-        },
-    });
-    
-    // err.output && err.output.statusCode
-    axios.mockRejectedValueOnce(new Error('Unknown error'));
     axios.mockRejectedValueOnce(new Error('Unknown error'));
     axios.mockRejectedValueOnce(new Error('Unknown error'));
     axios.mockRejectedValueOnce(new Error('Unknown error'));
 
-    axios.mockResolvedValue({
+    axios.mockResolvedValueOnce({
         data: {
             type: 'list',
             count: 1,
@@ -173,5 +134,86 @@ test('getTransactions', async () => {
     });
 
 	const result = await BasiqApi.getTransactions('c9f76ad8-491d-4a81-b68b-653672dfa6e7');
-    console.log(result)
+    
+    expect(result).toEqual([{
+        type: 'transaction',
+        id: '67985501-f0b1-489b-a460-7662e85be689',
+        status: 'posted',
+        description: 'Contribution - Super Guarantee',
+        amount: '609.69',
+        account: '44f7e034-a209-4dd0-ab6c-ef7f6f42440a',
+        balance: '0.00',
+        direction: 'credit',
+        class: 'transfer',
+        institution: 'AU00000',
+        connection: 'f8c4644e-a93d-4ea7-b1ff-59f595cda027',
+        enrich: null,
+        transactionDate: '',
+        postDate: '2023-04-13T00:00:00Z',
+        subClass: {
+            title: 'Superannuation Funds',
+            code: '633'
+        },
+        links: {
+            self: 'https://au-api.basiq.io/users/c9f76ad8-491d-4a81-b68b-653672dfa6e7/transactions/67985501-f0b1-489b-a460-7662e85be689',
+            account: 'https://au-api.basiq.io/users/c9f76ad8-491d-4a81-b68b-653672dfa6e7/accounts/44f7e034-a209-4dd0-ab6c-ef7f6f42440a',
+            institution: 'https://au-api.basiq.io/institutions/AU00000',
+            connection: null
+        }
+    }]);
+});
+
+test('getTransactions fails with 4 failed fetches', async () => {
+    // mock login
+    axios.mockResolvedValueOnce({
+        data: {
+            access_token: 'testtoken',
+            token_type: 'Bearer',
+            expires_in: 3600
+        }
+    });
+
+    axios.mockRejectedValueOnce(new Error('Unknown error'));
+    axios.mockRejectedValueOnce(new Error('Unknown error'));
+    axios.mockRejectedValueOnce(new Error('Unknown error'));
+    axios.mockRejectedValueOnce(new Error('Unknown error'));
+
+    axios.mockResolvedValueOnce({
+        data: {
+            type: 'list',
+            count: 1,
+            size: 1,
+            data: [
+                {
+                    type: 'transaction',
+                    id: '67985501-f0b1-489b-a460-7662e85be689',
+                    status: 'posted',
+                    description: 'Contribution - Super Guarantee',
+                    amount: '609.69',
+                    account: '44f7e034-a209-4dd0-ab6c-ef7f6f42440a',
+                    balance: '0.00',
+                    direction: 'credit',
+                    class: 'transfer',
+                    institution: 'AU00000',
+                    connection: 'f8c4644e-a93d-4ea7-b1ff-59f595cda027',
+                    enrich: null,
+                    transactionDate: '',
+                    postDate: '2023-04-13T00:00:00Z',
+                    subClass: {
+                        title: 'Superannuation Funds',
+                        code: '633'
+                    },
+                    links: {
+                        self: 'https://au-api.basiq.io/users/c9f76ad8-491d-4a81-b68b-653672dfa6e7/transactions/67985501-f0b1-489b-a460-7662e85be689',
+                        account: 'https://au-api.basiq.io/users/c9f76ad8-491d-4a81-b68b-653672dfa6e7/accounts/44f7e034-a209-4dd0-ab6c-ef7f6f42440a',
+                        institution: 'https://au-api.basiq.io/institutions/AU00000',
+                        connection: null
+                    }
+                }
+            ]
+        },
+    });
+
+	expect(BasiqApi.getTransactions('c9f76ad8-491d-4a81-b68b-653672dfa6e7'))
+    .rejects.toThrow(new Boom.notFound('Failed to connect to Basiq server'));
 });
