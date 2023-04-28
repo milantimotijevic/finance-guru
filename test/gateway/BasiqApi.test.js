12063d4ccb197ee1f3611ebe33ea40303f6b71c6
Milan Timotijevic <mtimo101289@gmail.com>
2023-04-28 19:57:38 +0200

    fix test

--- test/gateway/BasiqApi.test.js
+++ test/gateway/BasiqApi.test.js
@@ -78,7 +78,7 @@ test('getTransactions', async () => {
     }]);
 });
 
-test('getTransactions passes with 3 failed fetches', async () => {
+test('getTransactions passes with only a few failed fetches', async () => {
     axios.mockResolvedValueOnce({
         data: {
             access_token: 'testtoken',
@@ -155,7 +155,7 @@ test('getTransactions passes with 3 failed fetches', async () => {
     }]);
 });
 
-test('getTransactions fails with 4 failed fetches', async () => {
+test('getTransactions fails with too many failed fetches', async () => {
     axios.mockResolvedValueOnce({
         data: {
             access_token: 'testtoken',
@@ -167,41 +167,8 @@ test('getTransactions fails with 4 failed fetches', async () => {
     axios.mockRejectedValueOnce(new Error('Unknown error'))
     .mockRejectedValueOnce(new Error('Unknown error'))
     .mockRejectedValueOnce(new Error('Unknown error'))
-    .mockRejectedValueOnce(new Error('Unknown error'));
-
-    axios.mockResolvedValueOnce({
-        data: {
-            type: 'list',
-            count: 1,
-            size: 1,
-            data: [{
-                type: 'transaction',
-                id: '67985501-f0b1-489b-a460-7662e85be689',
-                status: 'posted',
-                description: 'Contribution - Super Guarantee',
-                amount: '609.69',
-                account: '44f7e034-a209-4dd0-ab6c-ef7f6f42440a',
-                balance: '0.00',
-                direction: 'credit',
-                class: 'transfer',
-                institution: 'AU00000',
-                connection: 'f8c4644e-a93d-4ea7-b1ff-59f595cda027',
-                enrich: null,
-                transactionDate: '',
-                postDate: '2023-04-13T00:00:00Z',
-                subClass: {
-                    title: 'Superannuation Funds',
-                    code: '633'
-                },
-                links: {
-                    self: 'https://au-api.basiq.io/users/c9f76ad8-491d-4a81-b68b-653672dfa6e7/transactions/67985501-f0b1-489b-a460-7662e85be689',
-                    account: 'https://au-api.basiq.io/users/c9f76ad8-491d-4a81-b68b-653672dfa6e7/accounts/44f7e034-a209-4dd0-ab6c-ef7f6f42440a',
-                    institution: 'https://au-api.basiq.io/institutions/AU00000',
-                    connection: null
-                }
-            }]
-        },
-    });
+    .mockRejectedValueOnce(new Error('Unknown error'))
+    .mockRejectedValueOnce(new Error('Unknown error'))
 
     expect(BasiqApi.getTransactions('c9f76ad8-491d-4a81-b68b-653672dfa6e7'))
         .rejects.toThrow(new Boom.notFound('Failed to connect to Basiq server'));
